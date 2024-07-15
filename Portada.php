a323625dd2488256b9755b6787b927a28a4b9dcd
CESARO2 <josecesarsanez@gmail.com>
2024-07-14 21:14:44 -0400

    Initial commit

---
+++ Portada.php
@@ -0,0 +1,69 @@
+<div class="carousel">
+            <div class="container-fluid">
+                    <div class="carousel-item">
+                        <div class="carousel-img">
+                            <img src="portada/Portada_index/img/carousel-3.jpg" alt="Image">
+                        </div>
+                        <div class="carousel-text">
+  <style>
+    .card-outline.card-primary {
+        background-color: rgba(255, 255, 255, 0.5);
+    }
+</style>
+<style>
+    .form-control,
+    .input-group-text,
+    .icheck-primary label {
+        border-radius: 10px;
+    }
+
+    button[name="login"] {
+        border-radius: 10px;
+    }
+</style>
+<div class="card card-outline card-primary">
+    <div class="card-header text-center">
+        <a href="#" class="h1"><b>Sistema </b> <b>Juridico</b> </a>
+    </div>
+    <div class="card-body">
+        
+
+        <form  action="login.php" method="post">
+            <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="Usuario"  name="username" value="admin" required="required" />
+                <div class="input-group-append">
+                    <div class="input-group-text">
+                        <span class="fas fa-envelope"></span>
+                    </div>
+                </div>
+            </div>
+            <div class="input-group mb-3">
+            <input type="password" class="form-control" placeholder="Password" name="password" value="admin" required="required" />
+                <div class="input-group-append">
+                    <div class="input-group-text">
+                        <span class="fas fa-lock"></span>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-8">
+                    <div class="icheck-primary">
+                        <input type="checkbox" id="remember">
+                        <label for="remember">
+                            recuerdame
+                        </label>
+                    </div>
+                </div>
+                <!-- /.col -->
+                <div class="col-4">
+                    <button type="submit"name="login" style="background-color: #0469FA; border: none; border-radius: 10px; padding: 5px 10px; color: white;">Iniciar</button>
+                </div>
+            </div>
+        </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    </div> 
+</div>
